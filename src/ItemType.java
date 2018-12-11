54f7e0734146cdd8373af4e5d0f0b54ed48479f8
brandonpalomino <brandonpalo@gmail.com>
2018-12-10 21:49:45 -0500

    OOP Program Release Candidate Reached around step 22

--- src/ItemType.java
+++ src/ItemType.java
@@ -1,19 +1,25 @@
 /**
  * Author: Brandon Palomino
- * Date: 9/28/2018
+ * Date: 9/29/2018
  * Class: OOP JAVA
  * Filename: ItemType.java
  *
  */
-
 
 //Create an enum called ItemType that will store
 // the following information. AUDIO, VISUAL, AudioMobile, VisualMobile
   // changed form on 10/26
 public enum ItemType {
 
-  AUDIO,
-  VI,
-  AM,
-  VM;
+  AUDIO("AU"),
+  VISUAL("VI"),
+  AUDIO_MOBILE("AM"),
+  VISUAL_MOBILE("VM");
+
+  public String code;
+
+  ItemType(String code) {
+    this.code = code;
+  }
 }
+
